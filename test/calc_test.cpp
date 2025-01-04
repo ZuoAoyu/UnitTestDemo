13c2bb53479506c13d8dbc2621ab5e7d5e514ab8
zuoaoyu <aoyu@xiake.me>
2025-01-04 21:23:23 +0800

    mock模拟

--- test/calc_test.cpp
+++ test/calc_test.cpp
@@ -1,9 +1,11 @@
 #include <gtest/gtest.h>
 #include "calc.h"
+#include "mocks/rng_mock.h"
 
 class CalcTestSuite : public ::testing::Test {
   protected:
-    Calc sut_;  // System Under Test
+    RandomNumberGeneratorMock rng_mock_;
+    Calc sut_{&rng_mock_};  // System Under Test
 };
 
 TEST_F(CalcTestSuite, SumAddsTwoInts) {
@@ -12,4 +14,9 @@ TEST_F(CalcTestSuite, SumAddsTwoInts) {
 
 TEST_F(CalcTestSuite, MultiplyMultipliesTwoInts) {
     EXPECT_EQ(6, sut_.Multiply(2, 3));
+}
+
+TEST_F(CalcTestSuite, AddRandomNumberAddsThree) {
+    EXPECT_CALL(rng_mock_, Get()).Times(1).WillOnce(::testing::Return(3));
+    EXPECT_EQ(4, sut_.AddRandomNumber(1));
 }
--- test/run_test.cpp
+++ test/run_test.cpp
@@ -2,6 +2,7 @@
 #include <sstream>
 #include <string>
 #include <iostream>
+#include <regex>
 
 int run(); // 前置声明
 
@@ -18,5 +19,18 @@ TEST(RunTest, RunOutputsCorrectEquations) {
   // restore original std::cout
   std::cout.rdbuf(prevCoutBuf);
 
-  EXPECT_EQ(expected, output);
+  EXPECT_EQ(expected, output.substr(0, output.find("3 + random number =")));
+
+  // 定义正则模式
+  std::regex pattern(R"(3 \+ random number = (\d+)\n)");
+  // 使用 std::regex_search 来检查 output 是否匹配模式
+  std::smatch matches;
+  bool match_found = std::regex_search(output, matches, pattern);
+  // 断言是否匹配
+  EXPECT_TRUE(match_found) << "Pattern not found in the output";
+  // 如果匹配，输出捕获的数字（随机数值）
+  if (match_found) {
+    int random_number = std::stoi(matches[1].str()); // 提取捕获的数字
+    std::cout << "Captured random number: " << random_number << std::endl;
+  }
 }
